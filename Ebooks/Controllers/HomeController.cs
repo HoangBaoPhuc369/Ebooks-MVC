dabdf8ba31257bca2d7447f4c623c2fae8afeb9a
HoangBaoPhuc369 <hoangbaophuc369@gmail.com>
2022-03-14 22:38:44 +0700

    Add project files.

---
+++ Ebooks/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Ebooks.Areas.Admin.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ebooks.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyAdminDataDataContext data = new MyAdminDataDataContext();
+        public ActionResult Index(int? page)
+        {
+            if (page == null) page = 1;
+            var all_sach = (from s in data.Books select s).OrderBy(m => m.id);
+            int pageSize = 5;
+            int pageNum = page ?? 1;
+            return View(all_sach.ToPagedList(pageNum, pageSize));
+        }
+    }
+}
